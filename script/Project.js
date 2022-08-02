f024e60450b150ccf40f48a7f414a6112f041bab
David Barath <davidbarath08@gmail.com>
2022-08-03 00:19:10 +0200

    working on responsive layouts ->  Home page

--- script/Project.js
+++ script/Project.js
@@ -1,1 +1,5 @@
 import {getElement,changetheme,getLocalTheme} from "./Home.js";
+
+//set the theme background height
+//press grid button -> see the  pictures with some writing->>> pinterest
+//grid button
