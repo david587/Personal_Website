d20e5ed30148727868068bdab8976393b5101d19
David Barath <davidbarath08@gmail.com>
2022-08-24 23:31:33 +0200

    home page menu apllying new styles

--- script/About.js
+++ script/About.js
@@ -7,7 +7,7 @@ import {getElement,changetheme,getLocalTheme} from "./Home.js";
 window.addEventListener("DOMContentLoaded", function () {
    //Viwe resume
 elements();
-// media();
+media();
 // a_myFunction(a); // Call listener function at run time
 //  a.addListener(a_myFunction); // Attach listener function on state changes
 
@@ -27,28 +27,15 @@ function openResume(){
     window.open("https://github.com/david587/Resume/blob/main/Bar%C3%A1th%20D%C3%A1vid%20%C3%B6n%C3%A9letrajz.pdf",'_blank'); 
 };
 
-// function media(){
-//     const line = document.getElementById("work_spn");
-//     window.innerWidth<400
-//     ?(line.innerHTML="-----")
-//     :(line.innerHTML="--------------------------");
-
-//     window.innerWidth>400 &&  window.innerWidth<500
-//     ?(line.innerHTML="---")
-//     :(line.innerHTML="--------------------------");
-// };
-
-let lista =[""]
-lista.forEach(element => {
+function media(){
     const line = document.getElementById("work_spn");
-    // var full = window.matchMedia("(max-width: 1284px)");
-    let el = "-";
-    let actual = window.innerWidth;
-    if(actual>actual){
-        lista.append(el);
-    }
-    console.log(lista);
-    
-    
-});
+    window.innerWidth<376
+    ?(line.textContent="->")
+    :(line.innerHTML="-------------------------------------------------------------------------------------------------");
+
+    // window.innerWidth>400 &&  window.innerWidth<500
+    // ?(line.innerHTML="---")
+    // :(line.innerHTML="--------------------------");
+};
+
 
--- script/Home.js
+++ script/Home.js
@@ -7,98 +7,94 @@ getLocalTheme();
 getElementMsg();
 localValue();
 
-
 //functions
- export function getElement(){
+export function getElement() {
     //elements
     const themeBtn = document.getElementById("themebtn");
     //events
-    themeBtn.addEventListener("click",changetheme);
-    }
- export function changetheme(){
+    themeBtn.addEventListener("click", changetheme);
+}
+export function changetheme() {
     let settheme = document.body;
-    settheme.classList.toggle("changetheme");
-   
+    settheme
+        .classList
+        .toggle("changetheme");
+
     var theme;
 
-    if(settheme.classList.contains("changetheme")){
+    if (settheme.classList.contains("changetheme")) {
         console.log("blue");
-        theme="BLUE";
-    }
-    else{
+        theme = "BLUE";
+    } else {
         console.log("NORMAL");
         theme = "NORMAL";
     }
     //set localstorage
-    localStorage.setItem("PageTheme", JSON.stringify(theme)); 
-    };
- export function getLocalTheme(){
+    localStorage.setItem("PageTheme", JSON.stringify(theme));
+};
+export function getLocalTheme() {
     //get theme locale
     let getTheme = JSON.parse(localStorage.getItem("PageTheme"));
-    if(getTheme === "BLUE"){
-        document.body.classList.toggle("changetheme")
+    if (getTheme === "BLUE") {
+        document
+            .body
+            .classList
+            .toggle("changetheme")
     };
 }
 
-//-------------------------------------------------------------
-
+// -------------------------------------------------------------
 
 //Get the messages from user
-function getElementMsg(){
-const textBtn = document.getElementById("send");
-//this if needs cause other html pages will drop error beacuse import
-if(document.body.contains(textBtn)){
-textBtn.addEventListener("click",takeMsg);
-}
+function getElementMsg() {
+    const textBtn = document.getElementById("send");
+    //this if needs cause other html pages will drop error beacuse import
+    if (document.body.contains(textBtn)) {
+        textBtn.addEventListener("click", takeMsg);
+    }
 };
-function takeMsg(){
+function takeMsg() {
     const textArea = document.querySelector("#input");
-    const InputValue=textArea.value;
-    const value = {InputValue}; 
-    let items = localStorage.getItem("InputValue")? //->null cause the first value  will not be there
-    JSON.parse(localStorage.getItem("InputValue"))
-    : [];
+    const InputValue = textArea.value;
+    const value = {
+        InputValue
+    };
+    let items = localStorage.getItem("InputValue")
+        ? //->null cause the first value  will not be there
+        JSON.parse(localStorage.getItem("InputValue"))
+        : [];
     items.push(value);
-    localStorage.setItem("InputValue",JSON.stringify(items)); 
+    localStorage.setItem("InputValue", JSON.stringify(items));
 };
 
-function localValue(){
-//get input value from local
-let getInput=JSON.parse(localStorage.getItem("InputValue"));
-if(getInput.length>5){
-    localStorage.removeItem("InputValue");
-}
+function localValue() {
+    //get input value from local
+    let getInput = JSON.parse(localStorage.getItem("InputValue"));
+    if (getInput.length > 5) {
+        localStorage.removeItem("InputValue");
+    }
 };
 
-function barButton(){
+function barButton() {
     // if(document.classList.contains("bar")){
     const barbtn = document.getElementById("bar");
-    if(document.body.classList.contains("")){}
+    if (document.body.classList.contains("")) {}
     const x = document.getElementById("x");
-    barbtn.addEventListener("click",openmodal)
-    x.addEventListener("click",closeModal);
-    
-    function openmodal(){
-       const modal=document.getElementsByClassName("modal");
-       modal[0].classList.toggle("open-modal");
+    barbtn.addEventListener("click", openmodal)
+    x.addEventListener("click", closeModal);
+
+    function openmodal() {
+        const modal = document.getElementsByClassName("modal");
+        // const body = document.getElementsByClassName("body");
+        modal[0].classList.toggle("open-modal");
+        // body[0].classList.toggle("blur");
+        // modal[0].classList.add("remblur");
     }
-    function closeModal(){
-        const modal=document.getElementsByClassName("modal");
-       modal[0].classList.toggle("open-modal");
+    function closeModal() {
+        const modal = document.getElementsByClassName("modal");
+        modal[0]
+            .classList
+            .toggle("open-modal");
     }
 }
 // }
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
