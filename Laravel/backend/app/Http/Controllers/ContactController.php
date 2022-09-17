8c9a065543e65bd94fb23ec9b7d77c6f6f0de402
David Barath <davidbarath08@gmail.com>
2022-09-18 00:22:35 +0200

    Form in laravel works,got the email

---
+++ Laravel/backend/app/Http/Controllers/ContactController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ContactMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function contact(){
+        return view("contact-us");
+    }
+
+    public function sendEmail(Request $request){
+        $details = [
+            "name" => $request->name,
+            "email"=> $request->email,
+            "message"=>$request->message
+        ];
+        Mail::to("davisbarath@gmail.com")->send(new ContactMail($details));
+        return back()->with("message_sent","Your message has beeen sent sucessfully!");
+    }
+}
